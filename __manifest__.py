a653083dbaaf969ba7e7c960766b4b715e5fab0f
Tilo <tilo.karczewski@gmx>
2022-05-30 17:57:39 +0200

    todo liste für Produktion geschrieben

--- __manifest__.py
+++ __manifest__.py
@@ -12,6 +12,11 @@
   'description': """
      - verkürzte Übernahme der Sale-Produktbeschreibung in Angebotspositionen, März 22
      - Erweiterung Produkt um Webshopeigenschaft, umgezogen in bbi_mod_stock in Mai 22
+     - TODO Idee Blendenmanagement, Lochbild als Produkt (eigene Kategorie Lochbild
+     mit hoher ID, eigene Kategorie Blende mit hoher ID ), Stückliste mit Rohling-Blende
+     und Lochbildern als BOM, rekursive Prüfung bei BOM.create() in dieser Kategorie, ob eine vergleichbare BOM schon existiert!
+     - TODO bbi Lagerort als Attribut ...bbi_location_id = fields.Many2one... mit eigener Lagerortentität
+     - TODO Eigenschaft Ersatzteil für Ersatzteil-Stücklisten Generator (bool)
     """,
   'depends': [
     'base',
