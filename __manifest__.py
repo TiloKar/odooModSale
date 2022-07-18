867f6774858f632ef9e6cffff521568bcdba3104
Tilo <tilo.karczewski@gmx>
2022-07-18 13:32:51 +0200

    fixing paperformat header spacings and dpi to 90

--- __manifest__.py
+++ __manifest__.py
@@ -23,9 +23,11 @@
   ],
   'data': [
     'report/bbi_external_layout.xml',
-    'report/minified_saleorder.xml',
     'report/bbi_report_saleorder.xml',
     'report/bbi_report_proforma.xml',
+    'report/paperformat_bbi.xml',
+    'report/minified_saleorder.xml',
+    'report/change_paperformat.xml',
     'data/sequense_saleorder_to_bzv.xml'
   ],
 }
