215aab9e5f4c15f7d9269d839d79350690e2cf6b
hnn@bbi-biotech.com <hnn@bbi-biotech.com>
2022-08-01 12:46:24 +0200

    push for hnn home pc

--- __manifest__.py
+++ __manifest__.py
@@ -30,8 +30,9 @@
     'report/change_paperformat.xml',
     'data/sequense_saleorder_to_bzv.xml',
     'views/sale_order_form_bbi.xml',
-    #'security/ir.model.access.csv',
-    #'security/fundamental_groups.xml',
-    #'security/fundamental_groups.xml',
+    'security/ir.model.access.csv',
+    'security/fundamental_groups.xml',
+    'security/fundamental_groups.xml',
+    'security/bbi_sale_security.xml',
   ],
 }
